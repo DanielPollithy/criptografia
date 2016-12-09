57a6542a6dc55cfd903f9caf3be2097b7f1fbe95
Daniel <pollithy.daniel@gmail.com>
2016-12-09 23:39:45 +0100

    Another todo

--- hill.py
+++ hill.py
@@ -1,2 +1,14 @@
+import random
+
 __author__ = 'Daniel'
 
+def generate_key(n):
+    while True:
+        k = [[random.randint(0, 255) for y in range(n)] for x in range(n)]
+        k_r = key_inverse(k)
+        if k_r:
+            return k, k_r
+
+def key_inverse(k):
+    # algorithm to inverse matrix...
+    pass
